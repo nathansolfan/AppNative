c6bf893a4162eb1e06f3e61e512480c65f1ef8f5
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-24 10:57:40 +0100

    update

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText} >Banana Quente</Text>
-      <Button title='Button'/>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your course goal'/>
+        <Button title='Add goal'/>
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    padding: 16,
-    borderColor: 'blue',
+  appContainer: {
+    padding: 50,d
   }
 });
+
+
+
+
+/* <View style={styles.container}>
+      <Text style={styles.dummyText} >Banana Quente</Text>
+      <Button title='Button'/>
+      <StatusBar style="auto" />
+</View> */
+
+// container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // dummyText: {
+  //   margin: 16,
+  //   borderWidth: 2,
+  //   padding: 16,
+  //   borderColor: 'blue',
+  // }
