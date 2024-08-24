ff4f1a6cd3dcd925d27bea69f35f82f4d90ef0d6
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-24 10:28:36 +0100

    Native fundamentals

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Banana Quente</Text>
+      <Button title='Button'/>
       <StatusBar style="auto" />
     </View>
   );
