dc5511e2bbf5f0a71e284b82800aee9e132d5da7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-25 10:25:18 +0100

    update

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  function goalInputHandler (enteredText) {
+    console.log(enteredText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal'/>
+        <TextInput 
+        style={styles.textInput} 
+        placeholder='Your course goal' 
+        onChangeText={goalInputHandler}/>
         <Button title='Add goal'/>
       </View>
-      <View>
-        <Text>List of goalss</Text>
+      <View style={styles.goalsContainer}>
+        <Text >List of goalssss</Text>
       </View>
     </View>
     
@@ -18,20 +26,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+
   inputContainer: {
+    flex: 1,
     // column is standard
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    
   },
+
   textInput: {
+    
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     // padding so text is not on the edge
     padding: 8
+  },
+
+  goalsContainer: {
+    flex: 5,
+
   }
 });
 
